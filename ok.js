f5015e1df73514cc50b52d7355516fd4ba918560
naysser <77415898+naysser@users.noreply.github.com>
2022-08-15 21:48:20 +0100

    Create ok.js

---
+++ ok.js
@@ -0,0 +1,5 @@
+const req = new XMLHttpRequest();
+req.open("GET", https://partner-marketing.bitdefender.com/hello);
+// retrieve data unprocessed as a binary string
+req.overrideMimeType("text/plain; charset=x-user-defined");
+/* ... */
